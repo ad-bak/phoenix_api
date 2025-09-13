c4e15061624cd1f178e0b7beeb14dfd24e025c68
adbak <adbak.dev@gmail.com>
2025-09-13 23:05:12 +0200

    users posts

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -4,4 +4,17 @@ defmodule ForumWeb.PageController do
   def home(conn, _params) do
     render(conn, :home)
   end
+
+  def users(conn, _params) do
+    IO.puts("User here")
+
+    users = [
+      %{id: 1, name: "Alice", email: "someone@gmail.com"},
+      %{id: 2, name: "John", email: "anyone@gmail.com"},
+      %{id: 3, name: "Bob", email: "bebe@gmail.com"},
+      %{id: 4, name: "Eve", email: "who@gmail.com"}
+    ]
+
+    json(conn, %{users: users})
+  end
 end
